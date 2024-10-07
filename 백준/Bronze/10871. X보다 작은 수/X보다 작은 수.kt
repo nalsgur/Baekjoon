2beee9db80bc12018c77db3995bf432edb8e43c2
나민혁 <138644365+nalsgur@users.noreply.github.com>
2024-10-07 12:43:15 +0900

    Update X보다 작은 수.kt

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val (n, x) = readLine()!!.split(" ").map { it.toInt() }
-    val numbers = readLine()!!.split(" ").map { it.toInt() }
+    val (n, x) = readLine()!!.split(" ").map { it.toInt() } // 첫 번째 줄 입력
+    val numbers = readLine()!!.split(" ").map { it.toInt() } // 두 번째 줄 입력
 
-    val result = numbers.filter { it < x } 
-    println(result.joinToString(" ")) 
+    val result = numbers.filter { it < x } // X보다 작은 수 필터링
+    println(result.joinToString(" ")) // 결과 출력
 }
